9deec94259c601cec29bd8de2c4e5bd036ad0424
rocky000552-star <rocky000552@gmail.com>
2025-10-31 22:41:37 +0530

    Add files via upload

--- script.d.ts
+++ script.d.ts
@@ -1,3 +1,19 @@
-import Script from './dist/client/script'
-export * from './dist/client/script'
-export default Script
+import React from 'react';
+import type { ScriptHTMLAttributes } from 'react';
+export interface ScriptProps extends ScriptHTMLAttributes<HTMLScriptElement> {
+    strategy?: 'afterInteractive' | 'lazyOnload' | 'beforeInteractive' | 'worker';
+    id?: string;
+    onLoad?: (e: any) => void;
+    onReady?: () => void | null;
+    onError?: (e: any) => void;
+    children?: React.ReactNode;
+    stylesheets?: string[];
+}
+/**
+ * @deprecated Use `ScriptProps` instead.
+ */
+export type Props = ScriptProps;
+export declare function handleClientScriptLoad(props: ScriptProps): void;
+export declare function initScriptLoader(scriptLoaderItems: ScriptProps[]): void;
+declare function Script(props: ScriptProps): JSX.Element | null;
+export default Script;
--- web-vitals.d.ts
+++ web-vitals.d.ts
@@ -1,1 +1,2 @@
-export * from './dist/client/web-vitals'
+import type { Metric } from 'next/dist/compiled/web-vitals';
+export declare function useReportWebVitals(reportWebVitalsFn: (metric: Metric) => void): void;
