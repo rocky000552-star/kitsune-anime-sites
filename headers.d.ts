348923f3810d2e233eecb160771657d14ab32992
rocky000552-star <rocky000552@gmail.com>
2025-10-31 22:48:00 +0530

    Add files via upload

--- headers.d.ts
+++ headers.d.ts
@@ -1,1 +1,5 @@
-export * from './dist/client/components/headers'
+import { type ReadonlyRequestCookies } from '../../server/web/spec-extension/adapters/request-cookies';
+import { DraftMode } from './draft-mode';
+export declare function headers(): import("../../server/web/spec-extension/adapters/headers").ReadonlyHeaders;
+export declare function cookies(): ReadonlyRequestCookies;
+export declare function draftMode(): DraftMode;
